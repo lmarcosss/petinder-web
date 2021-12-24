a7bd30523ff90a81c430f64558103c387d5d0bcd
Francisco Wolfarth Mossi <francisco.mossi@cwi.com.br>
2021-12-24 17:26:01 -0300

    exporting storage enum

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,11 +1,13 @@
 import { AnnouncementStatusEnum } from "./AnnouncementEnum";
 import { MenuEnum } from "./MenuEnum";
 import { FormLoginEnum, FormRegisterEnum, FormUserRegisterEnum } from "./FormEnum";
+import { StorageEnum } from "./StorageEnum";
 
 export {
   AnnouncementStatusEnum,
   MenuEnum,
   FormLoginEnum,
   FormRegisterEnum,
-  FormUserRegisterEnum
+  FormUserRegisterEnum,
+  StorageEnum
 };
