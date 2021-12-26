75af69358a765ed3d7f4567c179a093453f0416d
Francisco Wolfarth Mossi <francisco.mossi@cwi.com.br>
2021-12-25 21:48:05 -0300

    adding user type and api calls to profile page

--- src/services/auth/user.ts
+++ src/services/auth/user.ts
@@ -1,4 +1,5 @@
 import api from "./api";
+import { getAuthorizationHeader } from "@core";
 
 export function doRegister({
   name,
@@ -18,4 +19,30 @@ export function doRegister({
     email,
     password
   });
+}
+
+export function getUserInfo(req) {
+  const headers = getAuthorizationHeader(req);
+
+  return api.get("user/info", { headers });
+}
+
+export function saveUser({
+  cpf,
+  name,
+  phone,
+  birthDay,
+  email,
+  description
+}) {
+  const headers = getAuthorizationHeader();
+  
+  return api.put("user", {
+    cpf,
+    name,
+    phone,
+    birthDay,
+    email,
+    description
+  }, { headers });
 }
