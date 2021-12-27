5e1753f5c9589f0b3a50977def104cd30c849886
Leonardo Marcos <marcosleonardosss@gmail.com>
2021-12-27 18:07:02 -0300

    adjust env

--- src/services/auth/api.ts
+++ src/services/auth/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.PETINDER_AUTH_API
+  baseURL: process.env.NEXT_PUBLIC_PETINDER_AUTH_API
 });
--- src/services/petinder/api.ts
+++ src/services/petinder/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.PETINDER_API
+  baseURL: process.env.NEXT_PUBLIC_PETINDER_API
 });
