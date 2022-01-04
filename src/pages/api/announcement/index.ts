b1b74c737d3bf2d15593ba36897101688345e76d
Leonardo Marcos <marcosleonardosss@gmail.com>
2022-01-04 19:31:28 -0300

    remove api announcement

--- src/pages/api/announcement/index.ts
+++
@@ -1,7 +0,0 @@
-import { getOpennedAnnouncements } from "@services/petinder/announcement";
-
-export default async function announcement(req, res) {
-  const { data } = await getOpennedAnnouncements();
-
-  res.status(200).json(data);
-}
